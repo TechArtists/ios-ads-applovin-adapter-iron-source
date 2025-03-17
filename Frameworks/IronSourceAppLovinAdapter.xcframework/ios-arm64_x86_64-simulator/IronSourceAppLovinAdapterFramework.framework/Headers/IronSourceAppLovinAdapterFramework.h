23df6288dac8fb6043350b5dd33920d708aa3135
Tataru Robert <robert.tataru98@gmail.com>
2025-03-17 18:15:00 +0200

    First commit

---
+++ Frameworks/IronSourceAppLovinAdapter.xcframework/ios-arm64_x86_64-simulator/IronSourceAppLovinAdapterFramework.framework/Headers/IronSourceAppLovinAdapterFramework.h
@@ -0,0 +1,18 @@
+//
+//  IronSourceAppLovinAdapterFramework.h
+//  IronSourceAppLovinAdapterFramework
+//
+//  Created by Robert Tataru on 09.03.2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IronSourceAppLovinAdapterFramework.
+FOUNDATION_EXPORT double IronSourceAppLovinAdapterFrameworkVersionNumber;
+
+//! Project version string for IronSourceAppLovinAdapterFramework.
+FOUNDATION_EXPORT const unsigned char IronSourceAppLovinAdapterFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IronSourceAppLovinAdapterFramework/PublicHeader.h>
+
+
